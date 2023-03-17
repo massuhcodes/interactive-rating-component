3c1478661c0207a53f6a788b38f725289b8ba9d6
Farah Massuh <farahmassuh@Farahs-MBP.lan>
2023-03-16 23:35:44 -0400

    Fixed star icon issue

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,5 +1,7 @@
 // Star.jsx
 
+import star from "../assets/icon-star.svg"
+
 export default function Star() {
-    return <img src="./src/assets/icon-star.svg" alt="star icon" className="bg-[#252d37] rounded-full p-[1rem] w-12 h-auto" />
+    return <img src={star} alt="star icon" className="bg-[#252d37] rounded-full p-[1rem] w-12 h-auto" />
 }
